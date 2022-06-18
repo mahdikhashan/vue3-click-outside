f675b5c48a3060ea843730d38d513e07c5fca330
Mahdi Khashan <mkhashan@pyango.ch>
2022-06-18 14:18:20 +0430

    add the directive to pause and play the video

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
-import Vue from 'vue'
+export const onPageVideo = {
+  inserted: function (el) {
+    
+    var hidden, visibilityChange;
+    if (typeof document.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and later support
+      hidden = "hidden";
+      visibilityChange = "visibilitychange";
+    } else if (typeof document.msHidden !== "undefined") {
+      hidden = "msHidden";
+      visibilityChange = "msvisibilitychange";
+    } else if (typeof document.webkitHidden !== "undefined") {
+      hidden = "webkitHidden";
+      visibilityChange = "webkitvisibilitychange";
+    }
 
-export default Vue.directive('on-page', {
-  inserted: function (el, binding, vnode) {
-    // directive
+    el.onPageVideo = function (event) {
+      if (document[hidden]) {
+        el.pause();
+      } else {
+        el.play();
+      }
+    }
+
+    // Warn if the browser doesn't support addEventListener or the Page Visibility API
+    if (typeof document.addEventListener === "undefined" || hidden === undefined) {
+      console.log("This demo requires a browser, such as Google Chrome or Firefox, that supports the Page Visibility API.");
+    } else {
+      // Handle page visibility change
+      document.addEventListener(visibilityChange, el.onPageVideo, false);
+    }
   }
-})
+}
+
+export default onPageVideo;
